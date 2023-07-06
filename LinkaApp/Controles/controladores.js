5b1416e6751b614a3a27c223a09ecdd46b3dac98
Pedro PM Dias <diaspedro.dev@gmail.com>
2023-07-06 01:30:43 -0300

    novidades no js com node

--- LinkaApp/Controles/controladores.js
+++ LinkaApp/Controles/controladores.js
@@ -1,106 +1,117 @@
 // Projeto Leitor de Arquivos e Pastas
 
-const estrutura = {
-    'pastaPrincipal': {
-        'nome': 'LinkaApp',
-        'arquivos': ['dados.json', 'LinkaRemember.html'],
-        'pastas_filhas': {
-            'pastaFilha1': { 'nome': 'Controles', 'arquivos': ['controladores.js'] },
-            'pastaFilha2': { 'nome': 'Estilos', 'arquivos': ['estilizacao.css'] },
-            'pastaFilha3': { 'nome': 'Icones', 'arquivos': ['html-5.png', 'css-3.png', 'js.png', 'json.png'] }
+$(document).ready(() => {
+
+    const criarUl = (classes) => {
+        const ul = document.createElement('ul')
+        ul.classList.add('list-group')
+        if (Array.isArray(classes)) {
+            ul.classList.add(...classes);
+        }
+        return ul
+    }
+
+    const criarLi = (classes) => {
+        const li = document.createElement('li')
+        li.classList.add('list-group-item')
+        if (Array.isArray(classes)) {
+            li.classList.add(...classes);
+        }
+        return li
+    }
+
+    const criarKbd = (texto) => {
+        const kbd = document.createElement('kbd')
+        kbd.textContent = texto
+        return kbd
+    }
+
+    const criarH2 = (nome, classes) => {
+        const h2 = document.createElement('h2')
+        h2.textContent = nome
+        if (Array.isArray(classes)) {
+            h2.classList.add(...classes);
+        }
+        return h2
+    }
+
+    const criarH3 = (nome, classes) => {
+        const h3 = document.createElement('h3')
+        h3.textContent = nome
+        if (Array.isArray(classes)) {
+            h3.classList.add(...classes);
+        }
+        return h3
+    }
+
+    const estrutura = {
+        'projeto': {
+            'nome': 'LinkaApp',
+            'arquivos': ['dados.json', 'LinkaRemember.html'],
+            'pastas_filhas': {
+                'pastaFilha1': { 'nome': 'Controles', 'arquivos': ['controladores.js'] },
+                'pastaFilha2': { 'nome': 'Estilos', 'arquivos': ['estilizacao.css'] },
+                'pastaFilha3': { 'nome': 'Icones', 'arquivos': ['html-5.png', 'css-3.png', 'js.png', 'json.png'] }
+            }
         }
     }
-}
 
-const criarElementoUl = classes => {
-    const ul = document.createElement('ul')
-    ul.classList.add(...classes)
-    return ul
-}
+    const criarEstruturaHTML = (pastaProjeto) => {
+        const ulPai = criarUl()
+        ulPai.id = 'pastaProjeto'
 
-const criarElementoLi = classes => {
-    const li = document.createElement('li')
-    li.classList.add(...classes)
-    return li
-}
+        const liPai = criarLi()
+        const h2PastaPai = criarH2(pastaProjeto.projeto.nome, ['pasta_pai', 'badge'])
+        liPai.appendChild(h2PastaPai)
 
-const criarElementoKbd = texto => {
-    const kbd = document.createElement('kbd')
-    kbd.textContent = texto
-    return kbd
-}
+        const ulFilha = criarUl(['bg-warning'])
+        let indicePasta = 1;
+        for (let pasta in pastaProjeto.projeto.pastas_filhas) {
+            const pastaFilha = pastaProjeto.projeto.pastas_filhas[pasta];
 
-const criarElementoH2 = (nome, classes) => {
-    const h2 = document.createElement('h2')
-    h2.textContent = nome
-    h2.classList.add(...classes)
-    return h2
-}
+            let classePastaLi = `pastaPai_filha${indicePasta}`
+            const liFilha = criarLi([classePastaLi])
+            const h3PastaFilho = criarH3(pastaFilha.nome, [classePastaLi, 'h3', 'badge'])
+            const ulVovo = criarUl()
 
-const criarEstruturaHTML = () => {
-    const ulPai = criarElementoUl(['list-group'])
-    ulPai.id = 'pastaProjeto'
+            let indiceArquivo = 1;
+            for (let arquivo of pastaFilha.arquivos) {
+                let classeArquivo = `arquivo${indiceArquivo}_pasta${indicePasta}`
+                const kbdArquivoFilho = criarKbd(arquivo)
+                const linhaArquivo = criarLi([classeArquivo])
+                linhaArquivo.appendChild(kbdArquivoFilho)
+                ulVovo.appendChild(linhaArquivo)
+                indiceArquivo++;
+            }
+            liFilha.appendChild(h3PastaFilho)
+            liFilha.appendChild(ulVovo)
+            ulFilha.appendChild(liFilha)
+            indicePasta++;
+        }
 
-    const liPai = criarElementoLi(['list-group-item'])
+        const arquivos = pastaProjeto.projeto.arquivos
+        for (let indicePai = 0; indicePai < arquivos.length; indicePai++) {
+            let classeLi = `arquivo${indicePai + 1}_pastaPai`
+            const liArquivoPai = criarLi(['list-group-item', classeLi])
 
-    const h2PastaPai = criarElementoH2(estrutura.pastaPrincipal.nome, ['pasta_pai', 'badge'])
+            let nomeArquivo = arquivos[indicePai]
+            const kbdArquivoPai = criarKbd(nomeArquivo)
 
-    const ulFilha = criarElementoUl(['list-group', 'bg-warning'])
+            liArquivoPai.appendChild(kbdArquivoPai)
+            ulFilha.appendChild(liArquivoPai)
+        }
 
-    for (let pastaFilha = ''; pastaFilha < estrutura.pastaPrincipal.pastas_filhas.length; pastaFilha++) {
-        const ulPastaFilha = criarElementoUl(['list-group'])
-        const liFilha = criarElementoLi(['list-group-item'])
-
-        const h3PastaFilha = document.createElement('h3')
-        h3PastaFilha.classList.add('pasta_filha1', 'badge')
-        h3PastaFilha.textContent = pastaFilha.nome
-
-        pastaFilha.arquivos.forEach((arquivo) => {
-            const liArquivo = criarElementoLi(['list-group-item', 'arquivo1_pasta1'])
-            const kbdArquivo = criarElementoKbd(arquivo)
-            liArquivo.appendChild(kbdArquivo)
-            ulPastaFilha.appendChild(liArquivo)
-        })
-
-        liFilha.appendChild(h3PastaFilha)
-        liFilha.appendChild(ulPastaFilha)
-        ulFilha.appendChild(liFilha)
+        liPai.appendChild(ulFilha)
+        ulPai.appendChild(liPai)
+        return ulPai
     }
 
-    estrutura.pastaPrincipal.arquivos.forEach((arquivo) => {
-        const liArquivoPai = criarElementoLi(['list-group-item', 'arquivo1_pastaPai'])
-        const kbdArquivoPai = criarElementoKbd(arquivo)
-        liArquivoPai.appendChild(kbdArquivoPai)
-        ulFilha.appendChild(liArquivoPai)
+    // Atualizar lista de arquivos
+    $('button#atualizarEstrutura').click(() => {
+        const elementoEstrutura = criarEstruturaHTML(estrutura)
+        const listaFake = elementoEstrutura //console.log('listaFake:', listaFake)
+        console.log($('ul#pastaProjeto'))
+        $('ul#pastaProjeto').html(listaFake)
+        $('div#status').html(listaFake)
     })
-
-    liPai.appendChild(h2PastaPai)
-    liPai.appendChild(ulFilha)
-    ulPai.appendChild(liPai)
-
-    return ulPai
-}
-
-// Atualizar lista de arquivos
-//const divFuncaoCRUD = document.getElementById('pastaProjeto')
-//divFuncaoCRUD.html(elementoEstrutura)
-
-const elementoEstrutura = criarEstruturaHTML(estrutura)
-comparar = '<ul class="list-group" id="pastaProjeto"><li class="list-group-item"><h2 class="pasta_pai badge">LinkaApp</h2><ul class="list-group bg-warning"><li class="list-group-item"><h3 class="pasta_filha1 badge">Controles</h3><ul class="list-group"><li class="list-group-item arquivo1_pasta1"><kbd>controladores.js</kbd></li></ul></li><li class="list-group-item"><div class="pasta_filha2 badge">Estilos</div><ul class="list-group"><li class="list-group-item arquivo2_pasta2"><kbd>estilizacao.css</kbd></li></ul></li><li class="list-group-item"><div class="pasta_filha3 badge">Icones</div><ul class="list-group"><li class="list-group-item arquivo1_pasta3"><kbd>html-5.png</kbd></li><li class="list-group-item arquivo2_pasta3"><kbd>css-3.png</kbd></li><li class="list-group-item arquivo3_pasta3"><kbd>js.png</kbd></li><li class="list-group-item arquivo4_pasta3"><kbd>json.png</kbd></li></ul></li><li class="list-group-item arquivo1_pastaPai"><kbd>dados.json</kbd></li><li class="list-group-item arquivo2_pastaPai"><kbd>LinkaRemember.html</kbd></li></ul></li></ul>'
-teste = elementoEstrutura.outerHTML
-
-if (teste == comparar) {
-    console.log(Igualzinho)
-} else {
-    console.log('ainda nao')
-    console.log(teste)
-    console.log(comparar)
-}
-
-/*
-$(document).ready(() => {
-    //$('#funcaoCRUD').html(JSON(estrutura))
-
-    $('button#atualizarEstrutura').click((estrutura) => {})
 })
-*/
