56f3538a9c19d74d1780f220101ddb67355a580a
Pedro PM Dias <diaspedro.dev@gmail.com>
2023-07-14 21:08:00 -0400

    incorporação projeto mobile

--- LinkaApp/Controles/controladores.js
+++ LinkaApp/Controles/controladores.js
@@ -1,117 +1,127 @@
 // Projeto Leitor de Arquivos e Pastas
 
-$(document).ready(() => {
 
-    const criarUl = (classes) => {
-        const ul = document.createElement('ul')
-        ul.classList.add('list-group')
-        if (Array.isArray(classes)) {
-            ul.classList.add(...classes);
+// Função para criar um elemento <ul>
+var ts_criarUl = function (classes) {
+    var _a;
+    var ul = document.createElement('ul');
+    ul.classList.add('list-group');
+    if (Array.isArray(classes)) {
+        (_a = ul.classList).add.apply(_a, classes);
+    }
+    else if (classes) {
+        ul.classList.add(classes);
+    }
+    return ul;
+};
+// Função para criar um elemento <li>
+var ts_criarLi = function (classes) {
+    var _a;
+    var li = document.createElement('li');
+    li.classList.add('list-group-item');
+    if (Array.isArray(classes)) {
+        (_a = li.classList).add.apply(_a, classes);
+    }
+    else if (classes) {
+        li.classList.add(classes);
+    }
+    return li;
+};
+// Função para criar um elemento <kbd>
+var ts_criarKbd = function (texto) {
+    var kbd = document.createElement('kbd');
+    kbd.textContent = texto;
+    return kbd;
+};
+// Função para criar um elemento <h2>
+var ts_criarH2 = function (nome, classes) {
+    var _a;
+    var h2 = document.createElement('h2');
+    h2.textContent = nome;
+    if (Array.isArray(classes)) {
+        (_a = h2.classList).add.apply(_a, classes);
+    }
+    else if (classes) {
+        h2.classList.add(classes);
+    }
+    return h2;
+};
+// Função para criar um elemento <h3>
+var ts_criarH3 = function (nome, classes) {
+    var _a;
+    var h3 = document.createElement('h3');
+    h3.textContent = nome;
+    if (Array.isArray(classes)) {
+        (_a = h3.classList).add.apply(_a, classes);
+    }
+    else if (classes) {
+        h3.classList.add(classes);
+    }
+    return h3;
+};
+// Objeto com a estrutura de dados
+var ts_estrutura = {
+    projeto: {
+        nome: 'LinkaApp',
+        arquivos: ['dados.json', 'LinkaRemember.html'],
+        pastas_filhas: {
+            pastaFilha1: { nome: 'Controles', arquivos: ['controladores.js'] },
+            pastaFilha2: { nome: 'Estilos', arquivos: ['estilizacao.css'] },
+            pastaFilha3: { nome: 'Icones', arquivos: ['html-5.png', 'css-3.png', 'js.png', 'json.png'] }
         }
-        return ul
     }
-
-    const criarLi = (classes) => {
-        const li = document.createElement('li')
-        li.classList.add('list-group-item')
-        if (Array.isArray(classes)) {
-            li.classList.add(...classes);
-        }
-        return li
-    }
-
-    const criarKbd = (texto) => {
-        const kbd = document.createElement('kbd')
-        kbd.textContent = texto
-        return kbd
-    }
-
-    const criarH2 = (nome, classes) => {
-        const h2 = document.createElement('h2')
-        h2.textContent = nome
-        if (Array.isArray(classes)) {
-            h2.classList.add(...classes);
-        }
-        return h2
-    }
-
-    const criarH3 = (nome, classes) => {
-        const h3 = document.createElement('h3')
-        h3.textContent = nome
-        if (Array.isArray(classes)) {
-            h3.classList.add(...classes);
-        }
-        return h3
-    }
-
-    const estrutura = {
-        'projeto': {
-            'nome': 'LinkaApp',
-            'arquivos': ['dados.json', 'LinkaRemember.html'],
-            'pastas_filhas': {
-                'pastaFilha1': { 'nome': 'Controles', 'arquivos': ['controladores.js'] },
-                'pastaFilha2': { 'nome': 'Estilos', 'arquivos': ['estilizacao.css'] },
-                'pastaFilha3': { 'nome': 'Icones', 'arquivos': ['html-5.png', 'css-3.png', 'js.png', 'json.png'] }
-            }
-        }
-    }
-
-    const criarEstruturaHTML = (pastaProjeto) => {
-        const ulPai = criarUl()
-        ulPai.id = 'pastaProjeto'
-
-        const liPai = criarLi()
-        const h2PastaPai = criarH2(pastaProjeto.projeto.nome, ['pasta_pai', 'badge'])
-        liPai.appendChild(h2PastaPai)
-
-        const ulFilha = criarUl(['bg-warning'])
-        let indicePasta = 1;
-        for (let pasta in pastaProjeto.projeto.pastas_filhas) {
-            const pastaFilha = pastaProjeto.projeto.pastas_filhas[pasta];
-
-            let classePastaLi = `pastaPai_filha${indicePasta}`
-            const liFilha = criarLi([classePastaLi])
-            const h3PastaFilho = criarH3(pastaFilha.nome, [classePastaLi, 'h3', 'badge'])
-            const ulVovo = criarUl()
-
-            let indiceArquivo = 1;
-            for (let arquivo of pastaFilha.arquivos) {
-                let classeArquivo = `arquivo${indiceArquivo}_pasta${indicePasta}`
-                const kbdArquivoFilho = criarKbd(arquivo)
-                const linhaArquivo = criarLi([classeArquivo])
-                linhaArquivo.appendChild(kbdArquivoFilho)
-                ulVovo.appendChild(linhaArquivo)
+};
+var ts_criarEstruturaHTML = function (pastaProjeto) {
+    var ulPai = criarUl();
+    ulPai.id = 'pastaProjeto';
+    var liPai = criarLi();
+    var h2PastaPai = criarH2(pastaProjeto.projeto.nome, ['pasta_pai', 'badge']);
+    liPai.appendChild(h2PastaPai);
+    var ulFilha = criarUl(['bg-warning']);
+    var indicePasta = 1;
+    for (var pasta in pastaProjeto.projeto.pastas_filhas) {
+        if (pastaProjeto.projeto.pastas_filhas.hasOwnProperty(pasta)) {
+            var pastaFilha = pastaProjeto.projeto.pastas_filhas[pasta];
+            var classePastaLi = "pastaPai_filha".concat(indicePasta);
+            var liFilha = criarLi([classePastaLi]);
+            var h3PastaFilho = criarH3(pastaFilha.nome, [classePastaLi, 'h3', 'badge']);
+            var ulVovo = criarUl();
+            var indiceArquivo = 1;
+            for (var _i = 0, _a = pastaFilha.arquivos; _i < _a.length; _i++) {
+                var arquivo = _a[_i];
+                var classeArquivo = "arquivo".concat(indiceArquivo, "_pasta").concat(indicePasta);
+                var kbdArquivoFilho = criarKbd(arquivo);
+                var linhaArquivo = criarLi([classeArquivo]);
+                linhaArquivo.appendChild(kbdArquivoFilho);
+                ulVovo.appendChild(linhaArquivo);
                 indiceArquivo++;
             }
-            liFilha.appendChild(h3PastaFilho)
-            liFilha.appendChild(ulVovo)
-            ulFilha.appendChild(liFilha)
+            liFilha.appendChild(h3PastaFilho);
+            liFilha.appendChild(ulVovo);
+            ulFilha.appendChild(liFilha);
             indicePasta++;
         }
-
-        const arquivos = pastaProjeto.projeto.arquivos
-        for (let indicePai = 0; indicePai < arquivos.length; indicePai++) {
-            let classeLi = `arquivo${indicePai + 1}_pastaPai`
-            const liArquivoPai = criarLi(['list-group-item', classeLi])
-
-            let nomeArquivo = arquivos[indicePai]
-            const kbdArquivoPai = criarKbd(nomeArquivo)
-
-            liArquivoPai.appendChild(kbdArquivoPai)
-            ulFilha.appendChild(liArquivoPai)
-        }
-
-        liPai.appendChild(ulFilha)
-        ulPai.appendChild(liPai)
-        return ulPai
     }
-
-    // Atualizar lista de arquivos
-    $('button#atualizarEstrutura').click(() => {
-        const elementoEstrutura = criarEstruturaHTML(estrutura)
-        const listaFake = elementoEstrutura //console.log('listaFake:', listaFake)
-        console.log($('ul#pastaProjeto'))
-        $('ul#pastaProjeto').html(listaFake)
-        $('div#status').html(listaFake)
-    })
-})
+    var arquivos = pastaProjeto.projeto.arquivos;
+    for (var indicePai = 0; indicePai < arquivos.length; indicePai++) {
+        var classeLi = "arquivo".concat(indicePai + 1, "_pastaPai");
+        var liArquivoPai = criarLi(['list-group-item', classeLi]);
+        var nomeArquivo = arquivos[indicePai];
+        var kbdArquivoPai = criarKbd(nomeArquivo);
+        liArquivoPai.appendChild(kbdArquivoPai);
+        ulFilha.appendChild(liArquivoPai);
+    }
+    liPai.appendChild(ulFilha);
+    ulPai.appendChild(liPai);
+    return ulPai;
+};
+// Atualizar lista de arquivos
+var ts_buttonAtualizarEstrutura = document.querySelector('button#atualizarEstrutura');
+buttonAtualizarEstrutura === null || buttonAtualizarEstrutura === void 0 ? void 0 : buttonAtualizarEstrutura.addEventListener('click', function () {
+    var _a, _b;
+    var elementoEstrutura = criarEstruturaHTML(estrutura);
+    var listaFake = elementoEstrutura.cloneNode(true);
+    console.log(document.querySelector('ul#pastaProjeto'));
+    (_a = document.querySelector('ul#pastaProjeto')) === null || _a === void 0 ? void 0 : _a.appendChild(listaFake);
+    (_b = document.querySelector('div#status')) === null || _b === void 0 ? void 0 : _b.appendChild(listaFake);
+});
